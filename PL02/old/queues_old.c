a4e2808dca695aeef7c5aadb32fb6b583f100dba
Baltasar Aroso <baltasararoso@Baltasars-MBP.lan>
2018-03-18 16:34:38 +0000

    histogram in dotCSV files done

--- PL02/old/queues_old.c
+++ PL02/old/queues_old.c
@@ -3,7 +3,6 @@
 #include <string.h>
 #include <time.h>
 #include <math.h>
-#include <sys/stat.h>
 #include "linked_list.h"
 
 #define EVENTS_LIST 1000000
@@ -11,8 +10,12 @@
 // type 1 - ARRIVAL | type 2 - DEPARTURE
 #define ARRIVAL 1
 #define DEPARTURE 2
-#define INTERVAL 300
-#define CSVFolder "DotCSV/"
+
+//#define lambda 0.2 // Arrival rate of each free customer
+//#define dm 2 // Average service time in minutes
+//#define m 8 // Number of concurrent service channels / servers
+//#define L 4 // Number of system buffer positions
+//#define K 20 // Population size of potential clients
 
 void collectData (double* p_lambda, double* p_dm, int* p_m, int* p_L, int* p_K) {
   fprintf(stdout, "\n\tLet's begin the Simulation of a waitiling list!\n\n");
@@ -40,17 +43,18 @@ void collectData (double* p_lambda, double* p_dm, int* p_m, int* p_L, int* p_K) 
 int saveInCSV(char* filename, int* histogram, int hist_size) {
   FILE *CSV;
   int i;
-  fprintf(stdout, "\n\tSaving the histogram in \"%s\"\n", filename);
+  fprintf(stdout, "Saving the histogram in the %s\n", filename);
 
   CSV = fopen(filename,"w+");
   if(CSV == NULL) {
     perror("fopen");
     return -1;
   }
-  fprintf(CSV, "Index, Delay\n");
+  fprintf(CSV, "Indice, Tempo Central, Número de Chegadas (total %d)\n", EVENTS_LIST);
   for (i = 0; i < hist_size; i++) {
-    fprintf(CSV, "%d, %d\n", i, histogram[i]);
+    fprintf(CSV, "%d, %lf, %d\n", i, (2*i+1)/(float)(hist_size*2), histogram[i]);
   }
+
   return 0;
 }
 
@@ -78,106 +82,99 @@ list * addNewEvent(double t, double lt, int type, list* event_list, double dm){
   return event_list;
 }
 
-void calcDelayProb(double t, int tot_delay, int* hist, int h_size) {
-  int num_delay = 0;
-  for (int i = 0; i < (int)t; i++) {
-    num_delay += hist[i];
-  }
-  printf("\tDelay: %d\n", num_delay);
-  printf("\tTotal delay: %d\n", tot_delay);
-  fprintf(stdout, "\tThe probability is Pa(a<%.3f) = %.3f%%\n", t, ((float)num_delay) / tot_delay * 100);
-
-}
-
 int main(int argc, char* argv[]) {
+
    double lambda = 0, dm = 0;
-   int m = 0, L = 0, K = 0, index = 0;
+   int m = 0, L = 0, K = 0;
 
    collectData(&lambda, &dm, &m, &L, &K);
    lambda = lambda / 60; // passing calls per hour to calls per minutes
 
-   double lambda_total, aux_time = 0, delay_time = 0, t = -1;
+   double lambda_total, depart_time = 0, aux_time = 0, delay_time = 0;
    int i, buffer_size = L, channels = 0, arrival_events = 0, buffer_events = 0, delay_count = 0, losses = 0;
-   int hist_size = INTERVAL, *histogram;
-   char filename[50];
+   //int hist_size = 1000, *histogram;
+   //char filename[50];
    list *events = NULL, *buffer = NULL;
 
    srand(time(NULL));
 
-   if (stat(CSVFolder, NULL) == -1) {
-     mkdir(CSVFolder, 0777);
-   }
-
-   if (!strcpy(filename, CSVFolder)){
+   /*if (!strcpy(filename, argv[1])){
      perror("strcpy");
      return -1;
-   }
+   }*/
 
-   strcat(filename, argv[1]);
-
-   fprintf(stderr, "filename = %s\n", filename);
-
-   histogram = (int*)calloc(hist_size, sizeof(int));
+   //histogram = (int*)calloc(hist_size, sizeof(int));
 
    events = add(events, ARRIVAL, 0);
 
   // We process the list of events
   for(i = 1; i < EVENTS_LIST; i++) {
-    aux_time = events->_time;
+
     if (events->type == ARRIVAL) { // Arrival event
+
       arrival_events++;  // counting the arrival events
       lambda_total = (K - channels - (L-buffer_size)) * lambda; // arrival rate of the system
+      aux_time = events->_time;
 
       if (channels < m) { // if the channels aren't full
         events = addNewEvent(events->_time, lambda_total, DEPARTURE, events, dm);
-        channels++; // A channel serves the call
+        channels++;
+
+        //print_elems(events);
+        //fprintf(stdout, "//// Channels: %d ////\n", channels);
+
       } else if (buffer_size > 0) { // if the buffer has space
           buffer = add(buffer, ARRIVAL, events->_time);
           buffer_size--;
           buffer_events++;
+
+          //fprint_elems(buffer);
+          //fprintf(stdout, "ºººº Event added in the BUFFER: %d ºººº\n", L - buffer_size);
+
       } else { // if the channels and buffer are occupied we get a lost event
         losses++;
+        //fprintf(stdout, "\t... Client lost: %d ...\n", losses);
       }
-      // Remove the actual arrival event and add a new one
+      // substitute the actual event to a new arrival event
       events = rem(events);
       events = addNewEvent(aux_time, lambda_total, ARRIVAL, events, dm);
 
     } else { // Departure event
+      aux_time = events->_time;
       events = rem(events);
-      if (buffer != NULL) { // As a channel is free now it can serve an event waiting in the buffer
-        events = addNewEvent(aux_time, lambda_total, DEPARTURE, events, dm);
+      //fprintf(stdout, "\t<<< Channels after departure: %d >>>\n", channels);
+      if (buffer != NULL) {
+        depart_time = aux_time + calcTime(lambda_total, DEPARTURE, dm);
+        events = add(events, DEPARTURE, depart_time);
 
         delay_time += aux_time - buffer->_time;
-        index = (int)((aux_time - buffer->_time)*60);
-        histogram[index > (hist_size - 1) ? hist_size - 1 : index]++;
         delay_count++;
 
         buffer = rem(buffer);
         buffer_size++;
-      } else { // If the buffer is empty
+      } else {
         channels--;
       }
     }
   }
 
+  /*
+  index = (arrival_time-old_time)/(1 / (float)(HIST_JUMP*lambda));
+  histogram[index > (hist_size - 1) ? hist_size - 1 : index]++;
+
   if(saveInCSV(filename, histogram, hist_size) < 0) {
     perror("saveInCSV");
     return -1;
   }
-  fprintf(stdout, "\tFile \"%s\" saved successfully\n\n", filename);
+  fprintf(stdout, "File %s saved successfully\n\n", filename);
 
-  fprintf(stdout, "\nProbability of blocking (loss = %d) of customers (total arrivals = %d): B = %.3f%%\n", losses, arrival_events, losses / (float)arrival_events * 100);
-  fprintf(stdout, "\nProbability of customer service delay (buffer_events = %d): Pa = %.3f%%\n", buffer_events, buffer_events / (float)arrival_events * 100);
-  fprintf(stdout, "\nAverage customer service delay (delay_time = %.3f): Am = %.3fmin = %.3fsec\n\n", delay_time, delay_time / (float)arrival_events, delay_time / (float)(arrival_events) * 60);
+  */
 
-  while(1){
-    fprintf(stdout, "\nPlease enter the value of seconds (t) to compute the probability of the delay being less than t seconds Pa(a<t) or '0' to end: ");
-    scanf("%lf", &t);
-    if (t == 0) {
-      break;
-    }
-    calcDelayProb(t, delay_count, histogram, hist_size);
-  }
+  fprintf(stdout, "\n\t\tProbability of blocking (loss = %d) of customers (total arrivals = %d): B = %.3f %%\n", losses, arrival_events, losses / (float)arrival_events * 100);
+  fprintf(stdout, "\n\t\tProbability of customer service delay (buffer_events = %d): Pa = %.3f %%\n", buffer_events, buffer_events / (float)arrival_events * 100);
+  fprintf(stdout, "\n\t\tAverage customer service delay (delay_time = %.3f): Am = %.3f min = %.3f sec\n\n", delay_time, delay_time / (float)(arrival_events), delay_time / (float)(arrival_events) * 60);
+  //printf("\nEnter the value of seconds (t) to compute the Pa under certain value: ");
+  //scanf("%d", t);
 
   return 0;
 }
